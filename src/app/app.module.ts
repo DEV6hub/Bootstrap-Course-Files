76adb673f41be9e3cbd82d25ee45b08b06fbeb58
Sagar_Patel <sagarp@dev6.com>
2018-05-07 17:05:02 -0400

    Login component starter code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,43 +7,24 @@ import { HomeComponent } from './components/home/home.component';
 
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './components/login/login.component';
-import { ShippingComponent } from './components/shipping/shipping.component';
-import { CatalogComponent } from './components/catalog/catalog.component';
-import { ShirtComponent } from './components/shirt/shirt.component';
-
-import { ShirtGenderPipe } from './filters/shirt-filter';
-// For material tabs
-import { MatTabsModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // For bootstrap tabs
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-
-
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'catalog', component: CatalogComponent }
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SignupComponent,
-    LoginComponent,
-    ShippingComponent,
-    CatalogComponent,
-    ShirtComponent,
-    ShirtGenderPipe
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    BrowserAnimationsModule,
-    MatTabsModule,
     NgbModule.forRoot()
   ],
   providers: [],
