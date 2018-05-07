a7e6acc52898c51b0cc2455036e495844f513df9
Sagar_Patel <sagarp@dev6.com>
2018-05-07 16:18:13 -0400

    Home component startup code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 
 import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './components/login/login.component';
-import { ShippingComponent } from './components/shipping/shipping.component';
-import { CatalogComponent } from './components/catalog/catalog.component';
-import { ShirtComponent } from './components/shirt/shirt.component';
 
-import { ShirtGenderPipe } from './filters/shirt-filter';
 // For material tabs
 import { MatTabsModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -23,21 +16,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'catalog', component: CatalogComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    SignupComponent,
-    LoginComponent,
-    ShippingComponent,
-    CatalogComponent,
-    ShirtComponent,
-    ShirtGenderPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
