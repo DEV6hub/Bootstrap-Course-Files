17d8f0e7d0a89a6bcc33e80d65b1c078c9b8f990
Sagar_Patel <sagarp@dev6.com>
2018-05-07 16:41:33 -0400

    Signup component starter code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { Routes, RouterModule } from '@angular/router';
-import { SignupComponent } from './components/signup/signup.component';
-import { LoginComponent } from './components/login/login.component';
-import { ShippingComponent } from './components/shipping/shipping.component';
-import { CatalogComponent } from './components/catalog/catalog.component';
-import { ShirtComponent } from './components/shirt/shirt.component';
 
-import { ShirtGenderPipe } from './filters/shirt-filter';
 // For material tabs
 import { MatTabsModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,20 +18,13 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'catalog', component: CatalogComponent }
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    SignupComponent,
-    LoginComponent,
-    ShippingComponent,
-    CatalogComponent,
-    ShirtComponent,
-    ShirtGenderPipe
+    HomeComponent
   ],
   imports: [
     BrowserModule,
