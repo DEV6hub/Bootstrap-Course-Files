07129ede483fcef99b848bdc29160254a007da86
Sagar_Patel <sagarp@dev6.com>
2018-05-08 10:42:27 -0400

    Catalog component starter code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShippingComponent } from './components/shipping/shipping.component';
-import { CatalogComponent } from './components/catalog/catalog.component';
-import { ShirtComponent } from './components/shirt/shirt.component';
-
-import { ShirtGenderPipe } from './filters/shirt-filter';
-// For material tabs
-import { MatTabsModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // For bootstrap tabs
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -25,7 +18,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'catalog', component: CatalogComponent }
+  /* { path: 'catalog', component: CatalogComponent } */
 ];
 
 @NgModule({
@@ -35,15 +28,10 @@ const routes: Routes = [
     SignupComponent,
     LoginComponent,
     ShippingComponent,
-    CatalogComponent,
-    ShirtComponent,
-    ShirtGenderPipe
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
-    BrowserAnimationsModule,
-    MatTabsModule,
     NgbModule.forRoot()
   ],
   providers: [],
